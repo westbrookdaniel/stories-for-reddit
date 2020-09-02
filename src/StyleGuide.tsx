74b9085f918cdab8bbf0eb5c98fbd27be806b12c
westy12dan <westy12dan@gmail.com>
2020-09-02 21:09:25 +0800

    working on style guide

--- src/StyleGuide.tsx
+++ src/StyleGuide.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { Heading, Text, VStack, Button, HStack } from '@chakra-ui/core'
+import {
+	Heading,
+	Text,
+	VStack,
+	Button,
+	HStack,
+	Input,
+	Box,
+	Badge,
+	AspectRatio,
+} from '@chakra-ui/core'
 import { FcDocument } from 'react-icons/fc'
-
+import { MdKeyboardArrowRight, MdMoreHoriz } from 'react-icons/md'
 export default function StyleGuide() {
 	return (
 		<>
@@ -23,10 +33,54 @@ export default function StyleGuide() {
 					Fugiat?
 				</Text>
 				<HStack>
-					<Button colorScheme="primary">Primary</Button>
+					<Button
+						colorScheme="primary"
+                        pr={2}
+                        size="md"
+						rightIcon={<MdKeyboardArrowRight size={25} />}
+					>
+						Primary
+					</Button>
 					<Button>Default</Button>
 					<Button variant="ghost">Ghost</Button>
 				</HStack>
+				<Input focusBorderColor="primary.300" placeholder="Input Text Here" />
+				<AspectRatio
+					ratio={1}
+					maxW="200px"
+					w="100%"
+					borderRadius="md"
+					boxShadow="md"
+				>
+					<VStack p={5} justify="space-between">
+						<Heading
+                            fontSize="lg"
+							mt="1"
+							fontWeight="semibold"
+                            as="h4"
+                            lineHeight="1.4em"
+						>
+							[HR] Monster Hunter Saga
+						</Heading>
+						<Box w="100%">
+							<Box d="flex" alignItems="baseline" pt={3}>
+								<Badge borderRadius="full" px="2" colorScheme="primary">
+									14 min
+								</Badge>
+							</Box>
+							<Box
+								d="flex"
+								mt="2"
+								w="100%"
+								justifyContent="space-between"
+								alignItems="center"
+							>
+								<MdMoreHoriz fontSize={25} color="primary.500" />
+								<MdKeyboardArrowRight size={25} color="primary.500" />
+							</Box>
+						</Box>
+					</VStack>
+				</AspectRatio>
 			</VStack>
 		</>
 	)
--- theme.js
+++ theme.js
@@ -1,9 +1,11 @@
-import theme from '@chakra-ui/theme'
+import { extendTheme } from '@chakra-ui/core'
+import { mode } from "@chakra-ui/theme-tools"
+// import {theme as ogTheme} from '@chakra-ui/theme'
 
-const storiesTheme = {
-	...theme,
+// console.log(ogTheme.colors.blue)
+
+const theme = extendTheme({
 	colors: {
-		...theme.colors,
 		primary: {
 			50: '#f6eeee',
 			100: '#e5cdcd',
@@ -17,18 +19,18 @@ const storiesTheme = {
 			900: '#110909',
 		},
 		gray: {
-            50: '#fafafa',
-            100: '#f5f5f5',
-            200: '#eeeeee',
-            300: '#e0e0e0',
-            400: '#bdbdbd',
-            500: '#9e9e9e',
-            600: '#757575',
-            700: '#616161',
-            800: '#323232',
-            900: '#212121',
+			50: '#fafafa',
+			100: '#f5f5f5',
+			200: '#eeeeee',
+			300: '#e0e0e0',
+			400: '#bdbdbd',
+			500: '#9e9e9e',
+			600: '#757575',
+			700: '#616161',
+			800: '#323232',
+			900: '#212121',
 		},
 	},
-}
+})
 
-export default storiesTheme
+export default theme
