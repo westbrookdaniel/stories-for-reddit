9bc8cc8773467ff9d87a29e5292c7381feef8d8d
westy12dan <westy12dan@gmail.com>
2020-09-10 20:50:35 +0800

    remove log

--- theme.js
+++ theme.js
@@ -1,8 +1,8 @@
 import { extendTheme } from '@chakra-ui/core'
 import { mode } from '@chakra-ui/theme-tools'
 
-import { theme as ogTheme } from '@chakra-ui/theme'
-console.log(ogTheme)
+// import { theme as ogTheme } from '@chakra-ui/theme'
+// console.log(ogTheme)
 
 const theme = extendTheme({
 	fonts: {
