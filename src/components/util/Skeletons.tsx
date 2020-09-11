6fb81e995b5694a1820b083d14be85d380d39ca1
westy12dan <westy12dan@gmail.com>
2020-09-11 21:08:42 +0800

    improve night mode

--- src/components/util/Skeletons.tsx
+++ src/components/util/Skeletons.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Skeleton } from '@chakra-ui/core'
+import { Skeleton, useColorMode } from '@chakra-ui/core'
 import Card from './Card'
 import { AnyObject } from '../../types'
 import { motion } from 'framer-motion'
@@ -11,13 +11,20 @@ export const SkeletonCards = ({
 	quanitity?: Number
 	motionProps: AnyObject
 }) => {
+	const { colorMode } = useColorMode()
+
+	const fadeColors = {
+		startColor: colorMode === 'dark' ? 'gray.800' : 'tan.600',
+		endColor: colorMode === 'dark' ? 'gray.900' : 'tan.500',
+	}
+
 	const cards: any[] = new Array(quanitity)
 
 	if (motionProps) {
 		for (let i = 0; i < cards.length; i++) {
 			cards[i] = (
 				<motion.div key={i} {...motionProps}>
-					<Skeleton startColor="tan.600" endColor="tan.500">
+					<Skeleton {...fadeColors}>
 						<Card />
 					</Skeleton>
 				</motion.div>
@@ -26,7 +33,7 @@ export const SkeletonCards = ({
 	} else {
 		for (let i = 0; i < cards.length; i++) {
 			cards[i] = (
-				<Skeleton key={i} startColor="tan.600" endColor="tan.500">
+				<Skeleton key={i} {...fadeColors}>
 					<Card />
 				</Skeleton>
 			)
