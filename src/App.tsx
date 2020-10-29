330bc1aa4aec3d4044bf85c03603f98bbd1b40fb
westy12dan <westy12dan@gmail.com>
2020-10-29 19:26:41 +0800

    optimise file sizes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Switch, Route, useLocation } from 'react-router-dom'
 import { Box, Spinner } from '@chakra-ui/core'
 import { AnimatePresence, motion } from 'framer-motion'
 
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
+const Header = lazy(() => import('./components/layout/Header'))
+const Footer = lazy(() => import('./components/layout/Footer'))
 
 const Home = lazy(() => import('./Home'))
 const Stories = lazy(() => import('./Stories'))
@@ -35,50 +35,52 @@ const App = () => {
 	}
 
 	return (
-		<Box d="flex" minH="100vh" flexDir="column" overflow="hidden">
-			<Header />
+		<div className="wrapper-box">
+			<Susp>
+				<Header />
 
-			<AnimatePresence exitBeforeEnter>
-				<MotionBox
-					flexGrow={1}
-					display="flex"
-					flexDir="column"
-					key={location.pathname}
-				>
+				<AnimatePresence exitBeforeEnter>
 					<MotionBox
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1, transition: { delay: 0.1 } }}
-						exit={{ opacity: 0 }}
-						transition={{ ease: 'easeInOut', duration: '0.1' }}
-						onAnimationStart={ScrollAfterDelay}
+						flexGrow={1}
 						display="flex"
 						flexDir="column"
-						flexGrow={1}
+						key={location.pathname}
 					>
-						<Susp>
-							<Switch location={location}>
-								<Route exact path="/" component={Home} />
-								<Route path="/stories/:id" component={SingleStory} />
-								<Route path="/subreddits/:id" component={AnySubreddits} />
-								<Route exact path="/stories" component={Stories} />
-								<Route exact path="/about" component={About} />
-								<Route exact path="/subreddits" component={Subreddits} />
-								<Route exact path="/profile" component={Profile} />
-								<Route exact path="/login" component={Login} />
-								<Route exact path="/signup" component={SignUp} />
-								<Route
-									exact
-									path="/forgotpassword"
-									component={ForgotPassword}
-								/>
-								<Route exact path="/update" component={Update} />
-							</Switch>
-						</Susp>
+						<MotionBox
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1, transition: { delay: 0.1 } }}
+							exit={{ opacity: 0 }}
+							transition={{ ease: 'easeInOut', duration: '0.1' }}
+							onAnimationStart={ScrollAfterDelay}
+							display="flex"
+							flexDir="column"
+							flexGrow={1}
+						>
+							<Susp>
+								<Switch location={location}>
+									<Route exact path="/" component={Home} />
+									<Route path="/stories/:id" component={SingleStory} />
+									<Route path="/subreddits/:id" component={AnySubreddits} />
+									<Route exact path="/stories" component={Stories} />
+									<Route exact path="/about" component={About} />
+									<Route exact path="/subreddits" component={Subreddits} />
+									<Route exact path="/profile" component={Profile} />
+									<Route exact path="/login" component={Login} />
+									<Route exact path="/signup" component={SignUp} />
+									<Route
+										exact
+										path="/forgotpassword"
+										component={ForgotPassword}
+									/>
+									<Route exact path="/update" component={Update} />
+								</Switch>
+							</Susp>
+						</MotionBox>
 					</MotionBox>
-				</MotionBox>
-			</AnimatePresence>
-			<Footer />
-		</Box>
+				</AnimatePresence>
+				<Footer />
+			</Susp>
+		</div>
 	)
 }
 
